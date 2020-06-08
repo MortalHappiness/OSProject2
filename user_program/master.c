1217d3e8b4ca91cd56e3b760cc9c3a06a73bf80a
MortalHappiness <b07901069@ntu.edu.tw>
2020-06-09 00:13:56 +0800

    Finish fcntl(with multiple sockets)

--- user_program/master.c
+++ user_program/master.c
@@ -11,71 +11,102 @@
 
 #define PAGE_SIZE 4096
 #define BUF_SIZE 512
-size_t get_filesize(const char* filename);//get the size of the input file
-
+void help_message(); // print the help message
+size_t get_filesize(const char* filename); // get the size of the input file
 
 int main (int argc, char* argv[])
 {
-	char buf[BUF_SIZE];
-	int i, dev_fd, file_fd;// the fd for the device and the fd for the input file
-	size_t ret, file_size, offset = 0, tmp;
-	char file_name[50], method[20];
-	char *kernel_address = NULL, *file_address = NULL;
-	struct timeval start;
-	struct timeval end;
-	double trans_time; //calulate the time between the device is opened and it is closed
+    char buf[BUF_SIZE];
+    int dev_fd, file_fd;// the fd for the device and the fd for the input file
+    size_t ret, file_size, offset = 0, tmp;
+    int n_files;
+    char file_name[50], method[20];
+    char *kernel_address = NULL, *file_address = NULL;
+    struct timeval start;
+    struct timeval end;
+    double trans_time; //calulate the time between the device is opened and it is closed
 
+    // Deal with input parameters
+    if (argc < 4)
+    {
+        help_message();
+        return 1;
+    }
+    n_files = atoi(argv[1]);
+    if (n_files <= 0 || n_files != argc - 3)
+    {
+        help_message();
+        return 1;
+    }
+    strcpy(method, argv[argc - 1]);
 
-	if( (dev_fd = open("/dev/master_device", O_RDWR)) < 0)
-	{
-		perror("failed to open /dev/master_device\n");
-		return 1;
-	}
-	gettimeofday(&start ,NULL);
-	if( (file_fd = open (file_name, O_RDWR)) < 0 )
-	{
-		perror("failed to open input file\n");
-		return 1;
-	}
+    // ==============================
 
-	if( (file_size = get_filesize(file_name)) < 0)
-	{
-		perror("failed to get filesize\n");
-		return 1;
-	}
+    if( (dev_fd = open("/dev/master_device", O_RDWR)) < 0)
+    {
+        perror("failed to open /dev/master_device\n");
+        return 1;
+    }
 
+    // TODO:
+    // Now, for each file I create a socket.
+    // Maybe we need to consider how to transmit all files with single socket.
 
-	if(ioctl(dev_fd, 0x12345677) == -1) //0x12345677 : create socket and accept the connection from the slave
-	{
-		perror("ioclt server create socket error\n");
-		return 1;
-	}
+    for (int i = 2; i < argc - 1; ++i)
+    {
+        strcpy(file_name, argv[i]);
+        if( (file_fd = open (file_name, O_RDWR)) < 0 )
+        {
+            perror("failed to open input file\n");
+            return 1;
+        }
 
+        if( (file_size = get_filesize(file_name)) < 0)
+        {
+            perror("failed to get filesize\n");
+            return 1;
+        }
 
-	switch(method[0])
-	{
-		case 'f': //fcntl : read()/write()
-			do
-			{
-				ret = read(file_fd, buf, sizeof(buf)); // read from the input file
-				write(dev_fd, buf, ret);//write to the the device
-			}while(ret > 0);
-			break;
-	}
+        gettimeofday(&start ,NULL);
+        if(ioctl(dev_fd, 0x12345677) == -1) //0x12345677 : create socket and accept the connection from the slave
+        {
+            perror("ioctl server create socket error\n");
+            return 1;
+        }
 
-	if(ioctl(dev_fd, 0x12345679) == -1) // end sending data, close the connection
-	{
-		perror("ioclt server exits error\n");
-		return 1;
-	}
-	gettimeofday(&end, NULL);
-	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
-	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+        switch(method[0])
+        {
+            case 'f': //fcntl : read()/write()
+                // read from the input file
+                while ((ret = read(file_fd, buf, sizeof(buf))) > 0)
+                {
+                    write(dev_fd, buf, ret); // write to the device
+                }
+                break;
 
-	close(file_fd);
-	close(dev_fd);
+            case 'm': // mmap
+                break;
 
-	return 0;
+            default:
+                fprintf(stderr, "Invalid method : %s\n", method);
+                return 1;
+        }
+
+        if(ioctl(dev_fd, 0x12345679) == -1) // end sending data, close the connection
+        {
+            perror("ioclt server exits error\n");
+            return 1;
+        }
+
+        gettimeofday(&end, NULL);
+        trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
+        printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+
+        close(file_fd);
+    }
+
+    close(dev_fd);
+    return 0;
 }
 
 size_t get_filesize(const char* filename)
@@ -84,3 +115,9 @@ size_t get_filesize(const char* filename)
     stat(filename, &st);
     return st.st_size;
 }
+
+void help_message()
+{
+    printf("Usage: ./master [NUM] [FILES] [fcntl | mmap]\n");
+}
+
--- user_program/slave.c
+++ user_program/slave.c
@@ -11,67 +11,105 @@
 
 #define PAGE_SIZE 4096
 #define BUF_SIZE 512
+void help_message();
+
+
 int main (int argc, char* argv[])
 {
-	char buf[BUF_SIZE];
-	int i, dev_fd, file_fd;// the fd for the device and the fd for the input file
-	size_t ret, file_size = 0, data_size = -1;
-	char file_name[50];
-	char method[20];
-	char ip[20];
-	struct timeval start;
-	struct timeval end;
-	double trans_time; //calulate the time between the device is opened and it is closed
-	char *kernel_address, *file_address;
+    char buf[BUF_SIZE];
+    int dev_fd, file_fd;// the fd for the device and the fd for the input file
+    size_t ret, file_size, data_size = -1;
+    int n_files;
+    char file_name[50];
+    char method[20];
+    char ip[20];
+    struct timeval start;
+    struct timeval end;
+    double trans_time; //calulate the time between the device is opened and it is closed
+    char *kernel_address, *file_address;
 
+    // Deal with input parameters
+    if (argc < 5)
+    {
+        help_message();
+        return 1;
+    }
+    n_files = atoi(argv[1]);
+    if (n_files <= 0 || n_files != argc - 4)
+    {
+        help_message();
+        return 1;
+    }
+    strcpy(method, argv[argc - 2]);
+    strcpy(ip, argv[argc - 1]);
 
-	if( (dev_fd = open("/dev/slave_device", O_RDWR)) < 0)//should be O_RDWR for PROT_WRITE when mmap()
-	{
-		perror("failed to open /dev/slave_device\n");
-		return 1;
-	}
-	gettimeofday(&start ,NULL);
-	if( (file_fd = open (file_name, O_RDWR | O_CREAT | O_TRUNC)) < 0)
-	{
-		perror("failed to open input file\n");
-		return 1;
-	}
+    // ==============================
 
-	if(ioctl(dev_fd, 0x12345677, ip) == -1)	//0x12345677 : connect to master in the device
-	{
-		perror("ioclt create slave socket error\n");
-		return 1;
-	}
+    if( (dev_fd = open("/dev/slave_device", O_RDWR)) < 0)//should be O_RDWR for PROT_WRITE when mmap()
+    {
+        perror("failed to open /dev/slave_device\n");
+        return 1;
+    }
 
-    write(1, "ioctl success\n", 14);
+    // TODO:
+    // Now, for each file I create a socket.
+    // Maybe we need to consider how to transmit all files with single socket.
 
-	switch(method[0])
-	{
-		case 'f'://fcntl : read()/write()
-			do
-			{
-				ret = read(dev_fd, buf, sizeof(buf)); // read from the the device
-				write(file_fd, buf, ret); //write to the input file
-				file_size += ret;
-			}while(ret > 0);
-			break;
-	}
+    for (int i = 2; i < argc - 2; ++i)
+    {
+        strcpy(file_name, argv[i]);
+        file_size = 0;
+        if( (file_fd = open (file_name, O_RDWR | O_CREAT | O_TRUNC)) < 0)
+        {
+            perror("failed to open input file\n");
+            return 1;
+        }
 
+        gettimeofday(&start ,NULL);
+        if(ioctl(dev_fd, 0x12345677, ip) == -1) //0x12345677 : connect to master in the device
+        {
+            perror("ioctl create slave socket error\n");
+            return 1;
+        }
 
+        switch(method[0])
+        {
+            case 'f'://fcntl : read()/write()
+                // read from the the device
+                while ((ret = read(dev_fd, buf, sizeof(buf))) > 0)
+                {
+                    write(file_fd, buf, ret); //write to the input file
+                    file_size += ret;
+                }
+                break;
 
-	if(ioctl(dev_fd, 0x12345679) == -1)// end receiving data, close the connection
-	{
-		perror("ioclt client exits error\n");
-		return 1;
-	}
-	gettimeofday(&end, NULL);
-	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
-	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+            case 'm': // mmap
+                break;
 
+            default:
+                fprintf(stderr, "Invalid method : %s\n", method);
+                return 1;
+        }
 
-	close(file_fd);
-	close(dev_fd);
-	return 0;
+        if(ioctl(dev_fd, 0x12345679) == -1)// end receiving data, close the connection
+        {
+            perror("ioclt client exits error\n");
+            return 1;
+        }
+
+        gettimeofday(&end, NULL);
+        trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
+        printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+
+        close(file_fd);
+    }
+
+    close(dev_fd);
+    return 0;
 }
 
+void help_message()
+{
+    printf("Usage: ./slave [NUM] [FILES] [fcntl | mmap] [IP_ADDRESS]\n");
+}
 
